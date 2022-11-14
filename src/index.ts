155f5931977c3de29ae32fc85f58a30ee942ac7c
Julien Barbay <julien.barbay@gmail.com>
2022-11-14 20:20:06 +0700

    v0.1.0

--- src/index.ts
+++ src/index.ts
@@ -3,27 +3,26 @@ import * as vscode from 'vscode';
 import { exec } from "child_process";
 
 async function execShell(cmd: string, cwd: string) {
-  return new Promise<string>((resolve, reject) => {
+  return new Promise<string[]>((resolve, reject) => {
     exec(cmd, { cwd }, (err, out) => {
       if (err) {
         reject(err);
       } else {
-        resolve(out.replace(/\n$/gi, ''));
+        resolve(out.replace(/\n$/gi, '').split('\n'));
       }
-      err ? reject(err) : resolve(out);
     });
   });
 }
 
 type CommandToolbox = {
-  $: (shell: string) => Promise<string>
+  $: (shell: string) => Promise<string[]>
   currentSha: string
 };
 
 function registerCommand(
   context: vscode.ExtensionContext,
   command: string,
-  callback: (toolbox: CommandToolbox) => Promise<void>
+  callback: (toolbox: CommandToolbox) => Promise<string | void>
 ) {
   const disposable = vscode.commands.registerCommand(command, async () => {
     const rootPath = vscode.workspace.workspaceFolders?.[0].uri.path;
@@ -34,16 +33,19 @@ function registerCommand(
     }
 
     try {
-      const oldSha = await execShell('git rev-parse --short HEAD', rootPath!);
+      const [oldSha] = await execShell('git rev-parse HEAD', rootPath!);
 
-      await callback({
+      const message = await callback({
         $: shell => execShell(shell, rootPath!),
-        currentSha: oldSha.replace('\n', ''),
+        currentSha: oldSha,
       });
 
-      const newSha = await execShell('git rev-parse --short HEAD', rootPath!);
-      vscode.window.showInformationMessage(`now at #${newSha}`);
+      if (message) {
+        const [newSha] = await execShell('git rev-parse --short HEAD', rootPath!);
+        vscode.window.showInformationMessage(`${message} (#${newSha})`);
+      }
     } catch (err) {
+      console.error(err)
       vscode.window.showErrorMessage(err as string);
     }
   });
@@ -54,33 +56,45 @@ function registerCommand(
 
 export function activate(context: vscode.ExtensionContext) {
   registerCommand(context, 'gitplay.rewind', async ({ $, currentSha }) => {
-    const rootSha = await $('git rev-list --max-parents=0 HEAD');
+    const [rootSha] = await $('git rev-list --max-parents=0 HEAD');
 
     if (currentSha !== rootSha) {
       await $('git clean -df');
       await $(`git checkout ${rootSha}`);
+      return 'moved to root commit';
     }
+
+    throw 'most backward reached'
   });
 
   registerCommand(context, 'gitplay.prev', async ({ $, currentSha }) => {
-    const rootSha = await $('git rev-list --max-parents=0 HEAD');
+    const [rootSha] = await $('git rev-list --max-parents=0 HEAD');
 
     if (currentSha !== rootSha) {
+      await $('git clean -df');
       await $('git checkout HEAD~1 -f');
+      return 'moved backward';
     }
+
+    throw 'most backward reached';
   });
 
   registerCommand(context, 'gitplay.next', async ({ $, currentSha }) => {
-    const lastSha = await $('git show-ref --hash');
+    const [lastSha] = await $('git show-ref --hash --heads');
     const revList = await $(`git rev-list --topo-order ${currentSha}..${lastSha}`);
-    const allShas = revList.split('\n').reverse();
+    const allShas = [...revList]
+      .reverse()
+      .filter(sha => sha.length)
 
-    const index = allShas.findIndex(sha => sha.startsWith(currentSha));
+    if (!!allShas.length) {
+      const nextSha = allShas[0];
 
-    if (index < allShas.length - 1) {
-      const nextSha = allShas[index + 1];
+      await $('git clean -df');
       await $(`git checkout ${nextSha} -f`);
+      return 'moved forward';
     }
+
+    throw 'most forward reached'
   });
 }
 
